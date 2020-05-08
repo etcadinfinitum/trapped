842fd08ef73b6cc74e71bfbf885b684016d79f06
Lizzy Kate <lizzy.presland@gmail.com>
2020-05-07 18:02:22 -0700

    Add scene manager (#8)
    * refactored workspace by adding different assets folders, started work on goal functionality.
    
    * Added goal and goal behavior
    
    * saved scene for testing with copied client
    
    * stepping on goal now switches to next scene if all players are within goal
    
    * updated editor build setting so scene will switch to next level
    
    * Added rough Maze1 level for demo purposes. TODO: On load new level, server thinks a new player connected, but it's the same player.
    
    * fixed otherPlayer prefab rotation and decreased size
    
    * refactored, on goal touched scene no longer switches, instead the player is teleported to new location out of camera view.
    
    * forgot to save scene before pushing
    
    Co-authored-by: Victor Avelar <victor.r.avelar@gmail.com>

---
+++ trapped/Assets/Scripts/OtherPlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherPlayerBehavior : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
