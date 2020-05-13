eefbfcf4f5b79c187768dc5c75239704f4adbe79
Victor Avelar <victor.r.avelar@gmail.com>
2020-05-13 01:26:32 -0700

    unspahgettied, made some functions pascal casing to conform to convention, clients now process broadcasted disconnections properly, client udids are saved to each client on updating movement function. TODO: make an easy way to access connected clients by join order within the global controller

--- node/app.js
+++ node/app.js
@@ -9,9 +9,9 @@ var players = {}
 // on new client connect
 wss.on('connection', function connection (client) {
   console.log("client connected")
-//  client.broadcastyest = false
   // type of 'clients' is Set, so we use .size instead of .length
   console.log('# of connected clients: ' + wss.clients.size);
+    
   // on new message recieved
   client.on('message', function incoming (data) {
     // get data from string
@@ -28,43 +28,36 @@ wss.on('connection', function connection (client) {
     }
     // save player udid to the client
     client.udid = udid
-    
-//    if(!client.broadcastyest){
-//        broadcastUID(client.udid, 1)
-//        client.broadcastyest = true;
-//    }
-    
   })
+  
   client.on('close', function incoming(code, reason) {
     console.log('client connection closed.\n\tcode: ' + code + '\n\treason: ' + reason);
-    console.log("idwas: " + this.udid)
-    console.log("stringified: " + JSON.stringify(this.udid))
+    console.log("Udid was: " + this.udid)
     broadcastUID(this.udid, 0)
     delete players[client.udid];
   })
 })
 
-//called when client connects and disconnects (mode 0 = disconnect, mode 1 = connect)
+//called when client disconnects (mode 0). More modes currently not implemented
 function broadcastUID(uid, modeType){
-    console.log("broadcastUID, (uid, mode): " + uid + " " +modeType)
+    //console.log("broadcastUID, (uid, mode): " + uid + " " +modeType)
     var message = {
         mode: modeType,
         id: uid
     }
-      // broadcast messages to all clients
-  wss.clients.forEach(function each (client) {
-    // filter disconnected clients
-    if (client.readyState !== WebSocket.OPEN) {
-        return;
-    }
-    // filter out current player by client.udid
-    var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
-    // create array from the rest
-    client.send(JSON.stringify(message))
+    // broadcast messages to all clients
+    wss.clients.forEach(function each (client) {
+      // filter disconnected clients
+      if (client.readyState !== WebSocket.OPEN) {
+          return;
+      }
+      // filter out current player by client.udid
+      var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
+      // create array from the rest
+      client.send(JSON.stringify(message))
   })
 }
 
-//{"players":[{"position":{"x":-9.82,"y":-0.49,"z":0},"timestamp":1589345653651}]}
 function broadcastUpdate () {
   // broadcast messages to all clients
   wss.clients.forEach(function each (client) {
@@ -76,11 +69,10 @@ function broadcastUpdate () {
     var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
     // create array from the rest
     var otherPlayersData = otherPlayers.map(udid => players[udid])
-    console.log(otherPlayersData);
     // send it
     client.send(JSON.stringify({players: otherPlayersData}))
   })
 }
 
 // call broadcastUpdate every 0.1s
-setInterval(broadcastUpdate, 2000)
+setInterval(broadcastUpdate, 100)
