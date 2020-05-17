1fab82a8d2ff5a71fe376f69de91629ac42349ce
Kyloman <kyloman@gmail.com>
2020-05-17 14:51:28 -0700

    Adding future implementation for multiplayer UI, nickname included, converted text to TextMeshPro (#31)
    * Added player name option in beginning scene and rerouted all the text stuff to TMP
    
    * Some strange random asset change

--- trapped/Assets/Scripts/ConnectScreenScripts/ConnectButton.cs
+++ trapped/Assets/Scripts/ConnectScreenScripts/ConnectButton.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 public class ConnectButton : MonoBehaviour
 {
     string ip;
+    private string name;
     public void ConnectButtonClick(InputField ipfield)
     {
         ip = ipfield.text;
         if (ip != "")
         {
+            name = GameObject.Find("SetNameText").GetComponent<Text>().text;
+            GameObject.Find("SetNameText").name = "AltNameText";
+
             GameObject.Find("GlobalController").GetComponent<GlobalBehavior>().SetIP(ip);
+            GameObject.Find("GlobalController").GetComponent<GlobalBehavior>().SetName(name);
             SceneManager.LoadScene(1);
 
         }
