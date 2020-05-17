180784727c32d7cce8f47edbff8dcf9758756326
Lizzy Kate <lizzy.presland@gmail.com>
2020-05-17 15:08:44 -0700

    Make IP field on initial scene optional (#32)
    The WebSocket functionality already contains reasonable defaults for
    the Unity Editor and WebGL builds respectively, and those defaults will
    be used if an IP is not specified in the initial connection scene.

--- trapped/Assets/Scripts/Multiplayer2D.cs
+++ trapped/Assets/Scripts/Multiplayer2D.cs
@@ -71,7 +71,7 @@ public class Multiplayer2D : MonoBehaviour
         w = new WebSocket(new Uri("ws://"+ip+":"+port));
 
         yield return StartCoroutine(w.Connect());
-        Debug.Log("CONNECTED TO WEBSOCKETS");
+        Debug.Log("CONNECTED TO WEBSOCKETS; IP is " + ip);
 
         // generate random ID to have idea for each client (feels unsecure)
         System.Guid myGUID = System.Guid.NewGuid();
