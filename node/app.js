5ffcca303369088e71b94b9dcbf4ace23d5e0d32
victor.r.avelar@gmail.com <victor.r.avelar@gmail.com>
2020-05-23 19:00:25 -0700

    HuntBehavior now gets the first player to connect to server for navagent destination

--- node/app.js
+++ node/app.js
@@ -18,7 +18,13 @@ wss.on('connection', function connection (client) {
   //assign client's connect order on connect
   client.order = connectOrder
   connectOrder++
-  setJoinOrder = false;
+  //send join order to client
+  var discomessage = {
+        mode: 1,
+        id: client.order
+    }
+  client.send(JSON.stringify(discomessage))
+  
   // on new message recieved
   client.on('message', function incoming (data) {
     // get data from string
@@ -45,6 +51,7 @@ wss.on('connection', function connection (client) {
     connectOrder--;
   })
 })
+
 
 //called when client disconnects (mode 0). More modes currently not implemented
 function broadcastUID(uid, modeType){
