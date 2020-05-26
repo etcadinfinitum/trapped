d3e97e575e28bc58753cac7ac08366ea85b951bc
Jayden Kang <jkang18@uw.edu>
2020-05-25 23:13:02 -0700

    Traps working properly. Poison and normal darts recognized properly. TODO: Modify player health value and add a damage over time mechanic for poison

---
+++ trapped/Assets/Scripts/DartBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D colliderObj){
+        string tag = colliderObj.gameObject.tag; 
+
+        if (tag == "Player") {
+            PlayerBehavior player = colliderObj.gameObject.GetComponent<PlayerBehavior>(); 
+            if (gameObject.tag == "Normal Dart"){
+                Debug.Log("Player hit with a normal dart"); 
+            } else if (gameObject.tag == "Poison Dart") {
+                Debug.Log("Player got the big sicc");
+            }
+        }
+        Destroy(gameObject); 
+    }
+}
