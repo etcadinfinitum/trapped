a66676d7ff299192055c3fd6a1758348837ced74
Kyloman <kyloman@gmail.com>
2020-06-05 01:40:08 -0700

    Adding in title screen/credits screen

---
+++ trapped/Assets/MenuButtons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject help;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showHelp()
+    {
+        help.SetActive(true);
+    }
+
+    public void hideHelp()
+    {
+        help.SetActive(false);
+    }
+
+    public void goPlay()
+    {
+        SceneManager.LoadScene("ConnectScreen");
+    }
+}
