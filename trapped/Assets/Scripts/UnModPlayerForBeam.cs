b803336b065f8389f2f14b0b8dc930d08c4943b3
dcwell <cwell.de@gmail.com>
2020-06-07 00:23:01 -0700

    lots of polishing
    - Bal beam ready to place
    - HUGE polish with decor on maps (made prefabs for each)
    - need to still place balance beam and dart traps if not done already

---
+++ trapped/Assets/Scripts/UnModPlayerForBeam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnModPlayerForBeam : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Exit Beam");
+        CircleCollider2D player = collision.GetComponent<CircleCollider2D>();
+        player.radius = .9f;
+    }
+
+}
