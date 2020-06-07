d225c714151d3e4191139f62a52a86a4af027779
Lizzy Presland <lizzy.presland@gmail.com>
2020-06-07 16:22:35 -0700

    Ensure node server has semicolons
    While I realize this feature is optional, I like it for its explicitness.

--- node/app.js
+++ node/app.js
@@ -1,35 +1,35 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 
 // create new websocket server
-const wss = new WebSocket.Server({port: 8000})
+const wss = new WebSocket.Server({port: 8000});
 
 // empty object to store all players
-var players = {}
+var players = {};
 
 // player number based on order of connection to server, increases on client connect, decreases on disconnect
-var connectOrder = 1
+var connectOrder = 1;
 
 // on new client connect
 wss.on('connection', function connection (client) {
-  console.log("client connected")
+  console.log("client connected");
   // type of 'clients' is Set, so we use .size instead of .length
-  console.log('# of connected clients: ' + wss.clients.size)
+  console.log('# of connected clients: ' + wss.clients.size);
     
   //assign client's connect order on connect
-  client.order = connectOrder
-  console.log("Client's join order: " + client.order)
-  connectOrder++
+  client.order = connectOrder;
+  console.log("Client's join order: " + client.order);
+  connectOrder++;
   //send join order to client
   var discomessage = {
         mode: 1,
         id: client.order
-    }
-  client.send(JSON.stringify(discomessage))
+  };
+  client.send(JSON.stringify(discomessage));
   
   // on new message recieved
   client.on('message', function incoming (data) {
     // get data from string
-    var [udid, x, y, z] = data.toString().split('\t')
+    var [udid, x, y, z] = data.toString().split('\t');
     // store data to players object
     players[udid] = {
       position: {
@@ -39,19 +39,19 @@ wss.on('connection', function connection (client) {
       },
       timestamp: Date.now(),      
       id: udid+client.order,
-    }
+    };
     // save player udid to the client
-    client.udid = udid
-  })
+    client.udid = udid;
+  });
   
   client.on('close', function incoming(code, reason) {
     console.log('client connection closed.\n\tcode: ' + code + '\n\treason: ' + reason);
-    console.log("Udid was: " + this.udid)
-    broadcastUID(this.udid + client.order, 0)
+    console.log("Udid was: " + this.udid);
+    broadcastUID(this.udid + client.order, 0);
     delete players[client.udid];
     connectOrder--;
-  })
-})
+  });
+});
 
 
 //called when client disconnects (mode 0). More modes currently not implemented
@@ -60,7 +60,7 @@ function broadcastUID(uid, modeType){
     var message = {
         mode: modeType,
         id: uid
-    }
+    };
     // broadcast messages to all clients
     wss.clients.forEach(function each (client) {
       // filter disconnected clients
@@ -68,10 +68,10 @@ function broadcastUID(uid, modeType){
           return;
       }
       // filter out current player by client.udid
-      var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
+      var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid);
       // create array from the rest
-      client.send(JSON.stringify(message))
-  })
+      client.send(JSON.stringify(message));
+  });
 }
 
 function broadcastUpdate () {
@@ -82,13 +82,13 @@ function broadcastUpdate () {
         return;
     }
     // filter out current player by client.udid
-    var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid)
+    var otherPlayers = Object.keys(players).filter(udid => udid !== client.udid);
     // create array from the rest
-    var otherPlayersData = otherPlayers.map(udid => players[udid])
+    var otherPlayersData = otherPlayers.map(udid => players[udid]);
     // send it
-    client.send(JSON.stringify({players: otherPlayersData}))
-  })
+    client.send(JSON.stringify({players: otherPlayersData}));
+  });
 }
 
 // call broadcastUpdate every 0.1s
-setInterval(broadcastUpdate, 100)
+setInterval(broadcastUpdate, 100);
