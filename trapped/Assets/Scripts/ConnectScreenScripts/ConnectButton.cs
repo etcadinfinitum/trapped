98572fa236e5a663ef8bd4bd4a3362258c0bd600
Lizzy Presland <lizzy.presland@gmail.com>
2020-06-08 15:27:56 -0700

    Revise layout and button invoke for connect screen

--- trapped/Assets/Scripts/ConnectScreenScripts/ConnectButton.cs
+++ trapped/Assets/Scripts/ConnectScreenScripts/ConnectButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -6,21 +7,71 @@ public class ConnectButton : MonoBehaviour
 {
     string ip;
     private string myName;
-    public void ConnectButtonClick(InputField ipfield, InputField joinCodeField)
-    {
+    private string gameCode;
+    private Button newSessionButton;
+    private Button currentSessionButton;
+    private InputField joinCodeField;
+    private InputField ipfield;
+    private GlobalBehavior globalBehavior;
+
+    void Start() {
+        newSessionButton = GameObject.Find("NewSessionButton").GetComponent<Button>();
+        currentSessionButton = GameObject.Find("CurrentSessionButton").GetComponent<Button>();
+        joinCodeField = GameObject.Find("JoinCode").GetComponent<InputField>();
+        ipfield = GameObject.Find("IPField").GetComponent<InputField>();
+        globalBehavior = GameObject.Find("GlobalController").GetComponent<GlobalBehavior>();
+
+        newSessionButton.onClick.AddListener(StartNewSession);
+        currentSessionButton.onClick.AddListener(JoinExistingSession);
+    }
+
+    public void StartNewSession() {
+        Debug.Log("Creating custom join code for new session.");
+        gameCode = generateGameCode();
+        Debug.Log("Set custom join code as " + gameCode);
+        ConnectButtonClick();
+    }
+
+    public void JoinExistingSession() {
+        Debug.Log("Attempting to parse existing session code.");
+        if (joinCodeField.text != "") {
+            Debug.Log("Setting user-supplied join code: " + joinCodeField.text);
+            gameCode = joinCodeField.text.ToUpper();
+            ConnectButtonClick();
+        } else {
+            Debug.LogError("No join code was available in the join code field! Bad!");
+            // do not create default or do anything else.
+        }
+    }
+
+    /**
+     * Generic button click action; only game code setting matters 
+     * for different buttons.
+     */
+    public void ConnectButtonClick() {
         FindObjectOfType<AudioManager>().Play("ButtonPress");
         ip = ipfield.text;
         if (ip != "")
         {
-            GameObject.Find("GlobalController").GetComponent<GlobalBehavior>().SetIP(ip);
+            globalBehavior.SetIP(ip);
         }
-        if (joinCodeField.text != "") {
-            Debug.Log("Setting custom join code: " + joinCodeField.text);
-            GameObject.Find("GlobalController").GetComponent<GlobalBehavior>().SetGameCode(joinCodeField.text);
-        }
+
+        globalBehavior.SetGameCode(gameCode);
+
         myName = GameObject.Find("SetNameText").GetComponent<Text>().text;
         GameObject.Find("SetNameText").name = "AltNameText";
-        GameObject.Find("GlobalController").GetComponent<GlobalBehavior>().SetName(myName);
+        globalBehavior.SetName(myName);
         SceneManager.LoadScene(1);
+    }
+
+    string generateGameCode() {
+        string s = "";
+        for (int i = 0; i < 4; i++) {
+            float flt = UnityEngine.Random.value;
+            int shift = Convert.ToInt32(Mathf.Floor(25 * flt));
+            char letter = Convert.ToChar(shift + 65);
+            s = s + letter.ToString();  
+        }
+        return s;
     }
 }
